0299fba279a14d1a5178b8d64d5d4280174d60a5
kazza <kaz.kanso@gmail.com>
2016-10-13 21:36:33 +0100

    added fan pin for pwm control

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -1198,7 +1198,7 @@
 
 #define LED_PIN            -1
 
-#define FAN_PIN            -1
+#define FAN_PIN            4
  #if FAN_PIN == 12 || FAN_PIN ==13
   #define FAN_SOFT_PWM
 #endif
@@ -2950,7 +2950,7 @@ DaveX plan for Teensylu/printrboard-type pinouts (ref teensylu & sprinter) for a
 #endif
 
 #define SENSITIVE_PINS {0, 1, X_STEP_PIN, X_DIR_PIN, X_ENABLE_PIN, X_MIN_PIN, X_MAX_PIN, Y_STEP_PIN, Y_DIR_PIN, Y_ENABLE_PIN, Y_MIN_PIN, Y_MAX_PIN, Z_STEP_PIN, Z_DIR_PIN, Z_ENABLE_PIN, Z_MIN_PIN, Z_MAX_PIN, PS_ON_PIN, \
-                        HEATER_BED_PIN, FAN_PIN,                  \
+                        HEATER_BED_PIN,                   \
                         _E0_PINS _E1_PINS _E2_PINS             \
                         analogInputToDigitalPin(TEMP_0_PIN), analogInputToDigitalPin(TEMP_1_PIN), analogInputToDigitalPin(TEMP_2_PIN), analogInputToDigitalPin(TEMP_BED_PIN) }
 
