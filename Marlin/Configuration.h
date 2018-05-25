511f386a47a9ea42ce311dc2a73752be382b89f3
kazza <kaz.kanso@gmail.com>
2018-05-25 20:21:15 +0100

    increased size of x axis by 2cm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -338,7 +338,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 140
+#define X_MAX_POS 160
 #define X_MIN_POS 10
 #define Y_MAX_POS 160
 #define Y_MIN_POS 10
