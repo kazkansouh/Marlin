1633e612b4061a18ff6cf97e6b556d0a8ae9ef8f
kazza <kaz.kanso@gmail.com>
2018-07-12 13:39:01 +0100

    changed bed geometry

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -338,10 +338,10 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 100
-#define X_MIN_POS 10
+#define X_MAX_POS 110
+#define X_MIN_POS 0
 #define Y_MAX_POS 160
-#define Y_MIN_POS 10
+#define Y_MIN_POS 0
 #define Z_MAX_POS 101
 #define Z_MIN_POS 0
 
